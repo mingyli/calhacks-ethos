82c34501bb1cee7cb078e00863e30c050647e60c
Matthew Soh <matthewsoh.cw@gmail.com>
2016-11-12 14:09:45 -0800

    Modified run call to use 0.0.0.0

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from watson_developer_cloud import AlchemyLanguageV1
 from watson_developer_cloud import PersonalityInsightsV3
 from watson_developer_cloud import WatsonException
 
-PORT = os.environ['PORT'] or 5000
+PORT = os.environ['PORT'] if ('PORT' in os.environ) else 5000
 API_KEY = os.environ['IBM_WATSON_API_KEY']
 API_USERNAME = os.environ['IBM_SERVICE_USERNAME']
 API_PASSWORD = os.environ['IBM_SERVICE_PASSWORD']
@@ -134,4 +134,4 @@ def build_sample_author():
 
 if __name__ == "__main__":
     build_sample_author()
-    app.run(port=PORT)
+    app.run(host='0.0.0.0', port=PORT)
