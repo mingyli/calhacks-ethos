a7e8e306b1a97e4dbb7f744b1a0b9f317d4e649e
ming li <mingyli34@gmail.com>
2016-11-12 11:02:46 -0800

    stop using actual ajax requests, use local test jsons instead

--- popup.js
+++ popup.js
@@ -4,8 +4,6 @@ $( document ).ready(function() {
         apikey = data.apikey;
         console.log(apikey);
     });
-    // alert("welcome");
-    // $("#status").text("hello world");
 
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         console.log(tabs[0].url);
@@ -13,42 +11,45 @@ $( document ).ready(function() {
         make_sentiment_request(tabs[0].url);
     });
 
-    // var url = window.location.href;
     function make_author_request(tab_url){
-        $.ajax({
-            url: "https://gateway-a.watsonplatform.net/calls/url/URLGetAuthors?apikey=" + apikey,
-            type: "POST",
-            data:{
-                // apikey: "aee14f13b4f1254971213bab9b2c86b41e9c5fad",
-                outputMode: "json",
-                url: tab_url,
-
-            },
-            cache: false,
-            success: function(data){
-                console.log(data)
-                var author = data.authors.names[0]
-                $("#author").append(author);
-            }
+        // $.ajax({
+        //     url: "https://gateway-a.watsonplatform.net/calls/url/URLGetAuthors?apikey=" + apikey,
+        //     type: "POST",
+        //     data:{
+        //         outputMode: "json",
+        //         url: tab_url,
+        //
+        //     },
+        //     cache: false,
+        //     success: function(data){
+        //         console.log(data)
+        //         var author = data.authors.names[0]
+        //         $("#author").append(author);
+        //     }
+        // });
+        $.getJSON("author.json", function(json) {
+            console.log(json);
         });
     }
 
     function make_sentiment_request(tab_url){
-        $.ajax({
-            url: "https://gateway-a.watsonplatform.net/calls/url/URLGetTextSentiment?apikey=" + apikey,
-            type: "POST",
-            data:{
-                outputMode: "json",
-                url: tab_url,
-            },
-            cache: false,
-            success: function(data){
-                console.log(data)
-                // var sentiment = Math.round(Math.abs(parseFloat(data.docSentiment.score)) * 10) / 10.0;
-                var sentiment = Math.abs(parseFloat(data.docSentiment.score)) * 20.0;
-                sentiment = sentiment.toFixed(2);
-                $("#sentiment").append(String(sentiment))
-            }
+        // $.ajax({
+        //     url: "https://gateway-a.watsonplatform.net/calls/url/URLGetTextSentiment?apikey=" + apikey,
+        //     type: "POST",
+        //     data:{
+        //         outputMode: "json",
+        //         url: tab_url,
+        //     },
+        //     cache: false,
+        //     success: function(data){
+        //         console.log(data)
+        //         var sentiment = Math.abs(parseFloat(data.docSentiment.score)) * 20.0;
+        //         sentiment = sentiment.toFixed(2);
+        //         $("#sentiment").append(String(sentiment))
+        //     }
+        // });
+        $.getJSON("sentiment.json", function(json) {
+            console.log(json);
         });
     }
 
