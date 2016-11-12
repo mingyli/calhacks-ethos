ab1fd9020db0294014a65fb4293a6f64ea5ae07f
ming li <mingyli34@gmail.com>
2016-11-12 14:15:39 -0800

    get author profile json, abstracted bars

--- popup.js
+++ popup.js
@@ -10,6 +10,7 @@ $( document ).ready(function() {
         console.log(tabs[0].url);
         make_author_request(tabs[0].url);
         make_sentiment_request(tabs[0].url);
+        make_profile_request();
     });
 
     function make_author_request(tab_url){
@@ -33,7 +34,7 @@ $( document ).ready(function() {
             var author = json.authors.names[0];
             $("#author").append(author);
         });
-    }
+    };
 
     function make_sentiment_request(tab_url){
         // $.ajax({
@@ -53,26 +54,53 @@ $( document ).ready(function() {
         // });
         $.getJSON("sentiment.json", function(json) {
             console.log(json);
-            var sentiment = Math.abs(parseFloat(json.docSentiment.score)) * 20.0;
-            sentiment = sentiment.toFixed(2);
-            
-            if (sentiment > 10) {
-                sentiment = 10
-            }
-            if (sentiment > 7) {
-                $("#article-bar").addClass("progress-bar-danger");
-            }
-            else if (sentiment > 3) {
-                $("#article-bar").addClass("progress-bar-warning");
-            }
-            else {
-                $("#article-bar").addClass("progress-bar-success");
-            }
-            
-            var sentiment_str = String(sentiment*10);
-            
-            $( "#article-bar" ).css("width",sentiment_str+"%");
-            $( "#article-bar" ).text(sentiment_str);
+            var article_bias = Math.abs(parseFloat(json.docSentiment.score)) * 200.0;
+            article_bias = article_bias.toFixed(2);
+
+            var str_value = change_bar_color(article_bias, "#article-bar");
+            $("#article-bar").text(str_value);
+
         });
-    }
+    };
+
+    function make_profile_request() {
+        // TODO get ajax from matthew server
+        $.getJSON("profile.json", function(json) {
+            console.log(json);
+            var author_bias = Math.abs(parseFloat(json.bias)) * 200.0;
+            author_bias = author_bias.toFixed(2);
+
+            var str_value = change_bar_color(author_bias, "#author-bias-bar");
+            $("#author-bias-bar").text("Bias: " + str_value);
+
+
+            var author_openness = Math.abs(parseFloat(json.openness)) * 200.0;
+            author_openness = author_openness.toFixed(2);
+
+            var str_value = change_bar_color(author_openness, "#author-open-bar");
+            $("#author-open-bar").text("Openness: " + str_value);
+        });
+
+    };
+
+
+    // takes a percentage value and the id of the html element and updates
+    // the appearance of the bar
+    function change_bar_color(value, id) {
+        if (value > 100) {
+            value = 100;
+        }
+        if (value > 70) {
+            $(id).addClass("progress-bar-danger");
+        }
+        else if (value > 30) {
+            $(id).addClass("progress-bar-warning");
+        }
+        else {
+            $(id).addClass("progress-bar-success");
+        }
+        var str = String(value);
+        $(id).css("width",str+"%");
+        return str
+    };
 });
