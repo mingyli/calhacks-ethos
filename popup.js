f4309e9e25ef33416469907a2cc8a8a01d47405b
Julia Luo <juliazhiyuan.luo@gmail.com>
2016-11-12 02:25:07 -0800

    jquery changes

--- popup.js
+++ popup.js
@@ -7,10 +7,13 @@ $( document ).ready(function() {
 
     // var url = window.location.href;
     $.ajax({
-      url: window.location.href,
+      url: "https://gateway-a.watsonplatform.net/calls/url/URLGetAuthors?apikey=aee14f13b4f1254971213bab9b2c86b41e9c5fad",
+      data:{
+        url: window.location.href
+      },
       cache: false,
-      success: function(html){
-        $("#status").append("---" + html + "___");
+      success: function(data){
+        $("#status").append("---" + data + "___");
       }
     });
 });
