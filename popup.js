913ee2c62050393e38a14ab7759781a415c8ef98
ming li <mingyli34@gmail.com>
2016-11-12 02:18:06 -0800

    jquery get ajax

--- popup.js
+++ popup.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
     // alert("welcome");
     $("body").append('Test');
     $("#status").text("hello world");
+
+
+    // var url = window.location.href;
+    $.ajax({
+      url: window.location.href,
+      cache: false,
+      success: function(html){
+        $("#status").append("---" + html + "___");
+      }
+    });
 });
+
+$("#status").text("outside func")
