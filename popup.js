e66e295597ff5647e44fcdeb4b520fbb853fe40d
ming li <mingyli34@gmail.com>
2016-11-12 03:07:00 -0800

    ajax request ibm successful

--- popup.js
+++ popup.js
@@ -4,18 +4,29 @@ $( document ).ready(function() {
     $("body").append('Test');
     $("#status").text("hello world");
 
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        console.log(tabs[0].url);
+        make_request(tabs[0].url);
+    });
 
     // var url = window.location.href;
-    $.ajax({
-      url: "https://gateway-a.watsonplatform.net/calls/url/URLGetAuthors?apikey=aee14f13b4f1254971213bab9b2c86b41e9c5fad",
-      data:{
-        url: window.location.href
-      },
-      cache: false,
-      success: function(data){
-        $("#status").append("---" + data + "___");
-      }
-    });
-});
+    function make_request(tab_url){
+        $.ajax({
+            url: "https://gateway-a.watsonplatform.net/calls/url/URLGetAuthors?apikey=aee14f13b4f1254971213bab9b2c86b41e9c5fad",
+            type: "POST",
+            data:{
+                // apikey: "aee14f13b4f1254971213bab9b2c86b41e9c5fad",
+                outputMode: "json",
+                url: tab_url,
 
-$("#status").text("outside func")
+            },
+            cache: false,
+            success: function(data){
+                console.log(data)
+                var authors = data.authors.names
+
+                // $("#status").append("---" + authors + "___");
+            }
+        });
+    }
+});
