8a002c9e7522315bffd28e05b3a39655610f1e6c
song-william <william.song39@gmail.com>
2016-11-12 23:41:10 -0800

    changed initial link scrap to use indexed for loop rather than for each

--- facebook-scraper.js
+++ facebook-scraper.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
    console.log(stories)
    var links = [];
 
-   //links.push($(stories[2]).find("a").attr("href"))
-   stories.each(function() {
+   for(var i = 0; i < stories.length; i++){
+        var story_links = $(stories[i]).find("a")
+        for(var j = 0; j < story_links.length; j++){
+            links.push($(story_links[j]).attr("href"))
+        }
+   }
+
+   /*stories.each(function() {
         var story_links = $(this).find("a")
         story_links.each(function() {
             links.push($(this).attr("href"));
         });
-   });
+   });*/
 
    console.log(links)
    for(var i = links.length -1; i >= 0 ; i--){
@@ -27,3 +33,4 @@ function is_outside_URL(url){
     var https_patt = /https?:/;
     return url.search(https_patt) > -1 && url.search(facebook_patt) == -1
 }
+
