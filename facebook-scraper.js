9e86de87078a20b3fec7ae945b78929c7261780c
Matthew Soh <matthewsoh.cw@gmail.com>
2016-11-13 02:30:08 -0800

    Added facebook objectivity bars

--- facebook-scraper.js
+++ facebook-scraper.js
@@ -1,35 +1,77 @@
 $(document).ready(function(){
+  if(window.location.hostname != "www.facebook.com") return;
+  init();
+});
 
+var lowest = 0;
+var IBM_WATSON_API_KEY = "4eb7a087879404f17ee265805231d3bc7116ca56"
+var ratings = 0;
+var urlRatingMap = {}
+
+function init(){
+  scanStories();
+  setInterval(scanStories, 2000);
+  $(window).scroll(function(){
+    var current = $(document).scrollTop();
+    if(current - lowest > 250){
+      lowest = current;
+      scanStories();
+    }
+  });
+}
+
+function isOutsideURL(url){
+    var facebook_patt = /facebook.com/;
+    var https_patt = /https?:/;
+    return url.search(https_patt) > -1 && url.search(facebook_patt) == -1;
+}
+
+function addRating(stories, links){
+    for(var i = 0; i < stories.length; i++){
+        if($(stories[i]).find('.article-rating').length > 0) continue;
+        var ratingBar = "<div id = 'rating-bar-" + ratings + "' class = 'article-rating' style='width:100%;height:2em;box-shadow: 0px 2px 5px rgba(0,0,0,0.5);font-size:1.2em;z-index:1000;text-align:center;position:relative;padding:0.5em;color:white;box-sizing:border-box;opacity:0.9;'>Analysing article...</div>";
+
+        $(stories[i]).children().first().after(ratingBar);
+        urlRatingMap[links[i]] = ratings;
+        $.ajax({
+            url: "https://gateway-a.watsonplatform.net/calls/url/URLGetTextSentiment?apikey=" + IBM_WATSON_API_KEY,
+            type: "POST",
+            data:{
+                outputMode: "json",
+                url: links[i],
+            },
+            async: true,
+            success: function(data){
+                var articleObj = Math.max(100 - (Math.abs(parseFloat(data.docSentiment.score)) * 200.0), 0);
+                articleObj = articleObj.toFixed(2);
+                var ratingBarId = "#rating-bar-" + urlRatingMap[data.url];
+                var color = '#5cb85c';
+                if(articleObj < 70) color = '#f0ad4e';
+                if(articleObj < 30) color = '#c9302c';
+                $(ratingBarId).text("Objectivity - " + articleObj);
+                var currentStyle = $(ratingBarId).attr('style');
+                $(ratingBarId).attr('style', currentStyle + 'background-color:' + color + ';');
+            }
+        });
+        ratings++;
+    }
+}
+
+function scanStories(){
    var stories = $("div[role='article'][aria-label='Story']");
-   console.log(stories);
    var links = [];
    var article_stories = [];
-
    for(var i = 0; i < stories.length; i++){
+     if(($(stories[i]).find("div[role='article'][aria-label='Story']")).length > 0) continue;
         var story_links = $(stories[i]).find("a");
         for(var j = 0; j < story_links.length; j++){
             var link_str = $(story_links[j]).attr("href");
-            if(is_outside_URL(link_str)){
+            if(isOutsideURL(link_str)){
                 article_stories.push(stories[i]);
                 links.push(link_str);
                 break;
             }
         }
    }
-   console.log(article_stories)
-   console.log(links);
-   add_rating(article_stories)
-});
-
-function is_outside_URL(url){
-    var facebook_patt = /facebook.com/;
-    var https_patt = /https?:/;
-    return url.search(https_patt) > -1 && url.search(facebook_patt) == -1;
-}
-
-function add_rating(stories){
-    for(var i = 0; i < stories.length; i++){
-        var rect = "<div style='width:500px;height:100px;border:1px solid #000;''>This is a rectangle!</div>"
-        $(stories[i]).children().first().after(rect)
-    }
+   addRating(article_stories, links)
 }
