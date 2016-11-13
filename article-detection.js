fb0ba9a0ced0d32db43c8c46b81f80a7fa737266
Matthew Soh <matthewsoh.cw@gmail.com>
2016-11-12 23:40:32 -0800

    Added article detection

--- article-detection.js
+++ article-detection.js
@@ -1,13 +1,24 @@
 var DIFFBOT_TOKEN = "70ff6ee7380c5db201485b5f6da1f6b2"
-var client = new Diffbot(DIFFBOT_TOKEN);
+var client;
+
 function isArticle(url, callback){
-  client.analyze.get(
-    {url:url}, 
-    function onSuccess(response){
-      callback((response["type"] == "article"));
-    }, 
-    function onError(response){
-      console.log(response.error);
+  $.get("http://api.diffbot.com/v3/analyze", {
+    token:"70ff6ee7380c5db201485b5f6da1f6b2",
+    url:url
+  },
+    function success(data){
+      var article = (data["type"] == "article");
+      callback(article);
     }
   );
 }
+
+$(document).ready(function(){
+  isArticle(window.location.href, function(article){
+    if(article){
+      console.log("article!");
+    }else{
+      console.log("not article!");
+    }
+  });
+});
